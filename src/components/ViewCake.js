1cae2c45c9dc2b89ad77511d65629a12b3370f6b
Fraser Beaton <fraser0319@gmail.com>
2019-02-03 00:38:13 +0000

    updated view cake so it will load in the cake data if the url is hit for that certain cake id. I also updated it with a loading spinner while data is being retrieved

--- src/components/ViewCake.js
+++ src/components/ViewCake.js
@@ -8,44 +8,72 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { getCake } from '../actions';
+import { getCakes } from '../actions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import './../Card.css';
 
-const mapStateToProps = state => ({
-  cake: state.cakes[0]
-});
+function mapStateToProps(state) {
+  const { cakeId, cakeList } = state.cakes;
+  let selectedCake = {
+    currentCake: cakeList.find(cake => cake._id == cakeId)
+  };
+  return selectedCake;
+}
 
 class ViewCake extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props.match.params.id);
-    console.log(this.props.cake);
-    this.result = this.props.cake.filter(
-      cake => cake._id == this.props.match.params.id
-    );
+  async componentWillMount() {
+    if (this.props.currentCake == undefined) {
+      await this.props.dispatch(getCakes());
+    }
+    await this.props.dispatch(getCake(this.props.match.params.id));
   }
 
   render() {
+    let template;
+    if (this.props.currentCake !== undefined) {
+      template = (
+        <div>
+          <Card className="cakeCard">
+            <CardActionArea>
+              <CardMedia
+                image={this.props.currentCake.imageUrl}
+                style={{ height: 180 }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {this.props.currentCake.name}
+                </Typography>
+                <Typography color="textSecondary">
+                  Yum Factor {this.props.currentCake.yumFactor} / 5
+                </Typography>
+                <Typography color="textSecondary">
+                  {this.props.currentCake.comment}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <Button size="small" color="primary">
+                  Back
+                </Button>
+              </Link>
+            </CardActions>
+          </Card>
+          <FontAwesomeIcon icon={faCircleNotch} spin size="4x"/>
+        </div>
+      );
+    } else {
+      template = (
+        <div>
+          <FontAwesomeIcon icon={faCircleNotch} spin size="4x" />
+        </div>
+      );
+    }
     return (
       <div>
-        <Card className="cakeCard">
-          <CardActionArea>
-            <CardMedia image={this.result[0].imageUrl}style={{ height: 180 }} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {this.result[0].name}
-              </Typography>
-              <Typography color="textSecondary">Yum Factor {this.result[0].yumFactor} / 5</Typography>
-              <Typography color="textSecondary">{this.result[0].comment}</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-              <Button size="small" color="primary">
-                Back
-              </Button>
-            </Link>
-          </CardActions>
-        </Card>
+        {template}
       </div>
     );
   }

f631bc08a05bf1441c80cc45da7846075eaf97a0
Fraser Beaton <fraser0319@gmail.com>
2019-02-03 00:38:54 +0000

    updated the cake feed to work with the updated action structure

--- src/containers/CakeFeed.js
+++ src/containers/CakeFeed.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import CakeCard from '../components/CakeCard';
-import { getCake } from '../actions';
+import { getCakes } from '../actions';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
-const mapStateToProps = state => ({
-  cakes: state.cakes
-});
+// const mapStateToProps = state => ({
+//   cakes: state.cakes.cakeList
+// });
+
+function mapStateToProps(state) {
+  const { cakeList } = state.cakes;
+  return {
+    allCakes: cakeList
+  };
+}
 
 class CakeFeed extends React.Component {
   async componentDidMount() {
-    let response = await fetch(process.env.GET_CAKES_API_URL);
-    let json = await response.json();
-
-    this.props.dispatch(getCake(json));
+    await this.props.dispatch(getCakes());
   }
 
   render() {
     return (
       <div>
-      <Link to="/add-cake" style={{textDecoration: 'none'}}>
-      <Button size="large"  color="primary">Add New Cake</Button>
-      </Link>
-        {this.props.cakes[0] &&
-          this.props.cakes[0].map(cake => {
-            return (
-              <CakeCard
-                cake={cake}
-              />
-            );
+        <Link to="/add-cake" style={{ textDecoration: 'none' }}>
+          <Button size="large" color="primary">
+            Add New Cake
+          </Button>
+        </Link>
+        {this.props.allCakes &&
+          this.props.allCakes.map(cake => {
+            return <CakeCard cake={cake} />;
           })}
       </div>
     );

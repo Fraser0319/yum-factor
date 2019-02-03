9d3f0b23ed72657069e1f5ca5f278537c78b4c2c
Fraser Beaton <fraser0319@gmail.com>
2019-02-03 17:15:07 +0000

    made the top bar sticky

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,12 +2,9 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button';
 
 const TopBar = () => (
-  <AppBar position="static" className="topBar">
+  <AppBar position="sticky" className="topBar">
     <Toolbar>
       <Typography variant="h6" color="inherit">
         Cake App

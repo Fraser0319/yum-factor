f9caa928fabb3f4114dbd853b964e3235eb6f4f5
Fraser Beaton <fraser0319@gmail.com>
2019-02-03 22:15:03 +0000

    tidied up the form and added some basic validation

--- src/components/AddCake.js
+++ src/components/AddCake.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import { connect } from 'react-redux';
-
-import './../Card.css';
 import { setTitle } from '../actions';
 
 class AddCake extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { cakeName: '', comment: '', yumFactor: '', imageUrl: '' };
+    this.state = {
+      cakeName: '',
+      comment: '',
+      yumFactor: '',
+      imageUrl: '',
+      errors: ''
+    };
 
     this.handleChangeCakeName = this.handleChangeCakeName.bind(this);
     this.handleChangeComment = this.handleChangeComment.bind(this);
@@ -25,27 +27,47 @@ class AddCake extends React.Component {
     this.props.dispatch(setTitle('Add A New Cake'));
   }
 
+  validateForm() {
+    let errorMessage = [];
+    if (this.state.cakeName === '') {
+      errorMessage.push('You must enter a cake name');
+      console.log(errorMessage);
+    }
+    if (this.state.yumFactor === '') {
+      errorMessage.push('You must enter a yum factor');
+    }
+
+    if (errorMessage.length > 0) {
+      this.setState({
+        errors: errorMessage
+      });
+      return false;
+    }
+
+    return true;
+  }
+
   async handleSubmit(event) {
     event.preventDefault();
-    const request = new Request(process.env.ADD_NEW_CAKE_API_URL, {
-      credentials: 'same-origin',
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-      body: JSON.stringify({
-        name: this.state.cakeName,
-        comment: this.state.comment,
-        imageUrl:
-          'https://img.taste.com.au/9isesBer/taste/2016/11/caramello-cake-105070-1.jpeg',
-        yumFactor: this.state.yumFactor
-      })
-    });
+    if (this.validateForm()) {
+      const request = new Request(process.env.ADD_NEW_CAKE_API_URL, {
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({
+          name: this.state.cakeName,
+          comment: this.state.comment,
+          imageUrl:
+            'https://img.taste.com.au/9isesBer/taste/2016/11/caramello-cake-105070-1.jpeg',
+          yumFactor: this.state.yumFactor
+        })
+      });
 
-    const response = await fetch(request);
-    const result = await response.json();
-    console.log(result);
-    this.props.history.push('/');
-
-    // once done redirect to the home page.
+      const response = await fetch(request);
+      const result = await response.json();
+      console.log(result);
+      this.props.history.push('/');
+    }
   }
 
   handleChangeCakeName(event) {
@@ -66,77 +88,83 @@ class AddCake extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Card className="cakeCard">
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Add a new cake
-            </Typography>
-            <TextField
-              id="outlined-full-width"
-              label="Cake Name"
-              style={{ margin: 8 }}
-              placeholder="Cake Name"
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              InputLabelProps={{
-                shrink: true
-              }}
-              value={this.state.cakeName}
-              onChange={this.handleChangeCakeName}
-            />
-            <TextField
-              id="outlined-full-width"
-              label="Comment"
-              style={{ margin: 8 }}
-              placeholder="Comment"
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              InputLabelProps={{
-                shrink: true
-              }}
-              value={this.state.comment}
-              onChange={this.handleChangeComment}
-            />
-            <TextField
-              id="outlined-full-width"
-              label="Yum Factor"
-              style={{ margin: 8 }}
-              placeholder="Yum Factor"
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              InputLabelProps={{
-                shrink: true
-              }}
-              value={this.state.yumFactor}
-              onChange={this.handleChangeYumFactor}
-            />
-            <TextField
-              id="outlined-full-width"
-              label="Image URL"
-              style={{ margin: 8 }}
-              placeholder="Image URL"
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              InputLabelProps={{
-                shrink: true
-              }}
-              value={this.state.imageUrl}
-              onChange={this.handleChangeImageUrl}
-            />
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary" type="submit">Add Cake</Button>
-            <Link to="/" style={{textDecoration: 'none'}}><Button size="small" color="primary">Back</Button></Link>
-          </CardActions>
-        </Card>
+      <form onSubmit={this.handleSubmit} id="cakeForm">
+        {this.state.errors &&
+          this.state.errors.map(error => {
+            return <p style={{ color: 'red' }}>{error}</p>;
+          })}
+        <TextField
+          id="textInput"
+          error={this.state.name === ''}
+          label="Cake Name"
+          placeholder="Cake Name"
+          fullWidth
+          required
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={this.state.cakeName}
+          onChange={this.handleChangeCakeName}
+        />
+        <TextField
+          id="textInput"
+          label="Comment"
+          placeholder="Comment"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={this.state.comment}
+          onChange={this.handleChangeComment}
+        />
+        <FormControl />
+        <TextField
+          error={this.state.name === ''}
+          id="textInput"
+          label="Yum Factor"
+          placeholder="Yum Factor"
+          fullWidth
+          required
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={this.state.yumFactor}
+          onChange={this.handleChangeYumFactor}
+        />
+        <TextField
+          id="textInput"
+          label="Image URL"
+          placeholder="Image URL"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={this.state.imageUrl}
+          onChange={this.handleChangeImageUrl}
+        />
+        <div className="buttonGroup">
+          <Button
+            size="medium"
+            variant="contained"
+            color="primary"
+            type="submit"
+            id="addButton"
+          >
+            Add Cake
+          </Button>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Button size="medium" variant="contained" id="backButton">
+              Back
+            </Button>
+          </Link>
+        </div>
       </form>
     );
   }
 }
 
-export  default connect()(AddCake);
+export default connect()(AddCake);
